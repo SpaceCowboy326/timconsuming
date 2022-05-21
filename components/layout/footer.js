a9b846730aa145468ce2ef472d707e089690b82f
SpaceCowboy326 <timothy.hallowell@gmail.com>
2022-05-21 15:39:26 -0400

    Added GitHub link to footer content

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,19 +1,21 @@
 import { styled } from '@mui/system';
 import React from 'react';
 import { Box, Fade, Link, Paper, Typography } from '@mui/material';
+import Image from 'next/image'
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
-    alignItems: 'start',
+    alignItems: 'center',
     display: 'flex',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'start',
     width: '99%',
 }));
 
 const StyledLink = styled(Link)(({ theme }) => ({
-    color: theme.palette.background.default,
+    color: theme.palette.tertiary.main,
     fontWeight: 'bold',
     ':visited': {
-        color: theme.palette.tertiary.main,
+        color: theme.palette.background.default,
     }
 }));
 
@@ -23,13 +25,40 @@ function Footer({show, playerPanelShown}) {
     return (
         <Fade in={show} timeout={footerFadeDuration}>
             <Box sx={{display: 'flex', justifyContent: 'center', width: '100%'}}>
-                <StyledPaper sx={{height: playerPanelShown ? '5rem' : '3rem'}} elevation={3}>
+                <StyledPaper sx={{height: playerPanelShown ? '7rem' : '4.5rem'}} elevation={3}>
                     <Typography sx={{mt: '1em'}} variant="body2">
                         Nav Icons made by&nbsp;
                         <StyledLink underline="hover" target="_blank" rel="noopener noreferrer" href="https://www.freepik.com" title="Freepik">Freepik</StyledLink>&nbsp;
                         from&nbsp;
                         <StyledLink underline="hover" target="_blank" rel="noopener noreferrer" href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</StyledLink>&nbsp;
                     </Typography>
+                    <Link
+                        underline="hover"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://github.com/SpaceCowboy326"
+                        title="GitHub"
+                        sx={{
+                            alignItems: 'center',
+                            display: 'flex',
+                            mt: '.5em'
+                        }}
+                    >
+                        <Box sx={{height: '1.5em', width: '1.5em'}}>
+                            <Image
+                                height={64}
+                                layout="responsive"
+                                width={64}
+                                src={'/images/github-mark.png'}
+                            />
+                        </Box>
+                        <StyledLink
+                            sx={{
+                                ml: '.25em',
+                            }}>
+                            @SpaceCowboy326
+                        </StyledLink>
+                    </Link>
                 </StyledPaper>
             </Box>
         </Fade>
